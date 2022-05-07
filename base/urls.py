a61cfc6fd4b486f31dc2878f9abb49317a7017b1
Ayatoo047 <ajibadeyakubadebare@gmail.com>
2022-05-07 10:48:15 +0100

    init

---
+++ base/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from django.views import View
+from . import views
+
+
+urlpatterns = [
+    path('login/', views.loginRegister, name = "login"),
+    path('logout/', views.logoutuser, name = "logout"),
+    path('register/', views.registeruser, name = "register"),
+    path('', views.home, name = "home"),
+    path('room/<str:pk>/', views.room, name = "room"),
+    path('create-room/', views.createroom, name = "createroom"),
+    path('edit-room/<str:pk>/', views.updateroom, name = "editroom"),
+    path('delete-room/<str:pk>/', views.deleteroom, name = "deleteroom"),
+    path('delete-message/<str:pk>/', views.deletemessage, name = "deletemessage"),
+    path('user-profile/<str:pk>/', views.userprofile, name = "userprofile"),
+]
