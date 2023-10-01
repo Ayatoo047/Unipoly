a6498d77a9ce697a45b4068ae0a7a2f77e2fb84e
Ajibade Yakub <ajibadeyakubadeabre@gmail.com>
2023-10-01 21:13:04 +0100

    kk

--- unipoly/settings.py
+++ unipoly/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -184,4 +184,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['https://ft9ja-production.up.railway.app']
+# CSRF_TRUSTED_ORIGINS = ['https://ft9ja-production.up.railway.app']
