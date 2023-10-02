4dd732bef83f121c684099c3dd66a3a040808a09
Ajibade Yakub <ajibadeyakubadeabre@gmail.com>
2023-10-02 07:18:30 +0100

    j

--- unipoly/settings.py
+++ unipoly/settings.py
@@ -78,30 +78,35 @@ TEMPLATES = [
 WSGI_APPLICATION = 'unipoly.wsgi.application'
 ASGI_APPLICATION = 'unipoly.asgi.application'
 
-# CHANNEL_LAYERS = {
-#     "default": {
-#         "BACKEND": "channels_redis.core.RedisChannelLayer",
-#         "CONFIG": {
-#             "hosts": [("127.0.0.1", 6379)],
-#         },
-#     },
-# }
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.pubsub.RedisPubSubChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts":[{
-                "address": "redis://red-ckcudeciibqc73ej0p6g:6379",  # "REDIS_TLS_URL"
-                "ssl_cert_reqs": None,
-            }]
-        }
-    }}
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
 
 # CHANNEL_LAYERS = {
 #     "default": {
 #         "BACKEND": "channels.layers.InMemoryChannelLayer"
 #     }
 # }
+
+if DEBUG == False:
+    CHANNEL_LAYERS = {
+        "default": {
+            "BACKEND": "channels_redis.pubsub.RedisPubSubChannelLayer",
+            "CONFIG": {
+                "hosts":[{
+                    "address": "rediss://red-ckcudeciibqc73ej0p6g:SlLNzkkXoLCeZmIz0M8SzK4Dqnow5ZbE@oregon-redis.render.com:6379",  # "REDIS_TLS_URL"
+                    "ssl_cert_reqs": None,
+                    "on_disconnect": "redis.disconnect"
+                }]
+            }
+        }}
+
+
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
