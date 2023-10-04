8e43799a2b0df9845df064ba52a3d22a7c71f230
Ayatoo047 <101508422+Ayatoo047@users.noreply.github.com>
2023-10-04 11:03:53 +0100

    Update settings.py

--- unipoly/settings.py
+++ unipoly/settings.py
@@ -205,4 +205,3 @@ STATIC_ROOT = (str(os.path.join(BASE_DIR, 'static'),))
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CSRF_TRUSTED_ORIGINS = ['https://unipoly.onrender.com/']
